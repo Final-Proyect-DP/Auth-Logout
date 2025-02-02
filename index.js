071f812a41451c33ab7c4be729e58024710ea574
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-02-02 13:48:35 -0500

    feat: :sparkles: add dockerfile and cors to the project

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const logoutRoutes = require('./routes/logoutRoutes');
 const logger = require('./config/logger');
 const authLoginConsumer = require('./consumers/authLoginConsumer');
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Configuración de CORS
+const corsOptions = {
+  origin: '*', // En producción, especifica los orígenes permitidos
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const swaggerOptions = {
